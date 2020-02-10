f7913cc942330ecb0e7f7fb7808b0b4ab826c97e
durex3 <liugelong666@163.com>
2020-02-10 19:09:23 +0800

    完善广告检索服务

--- ad-service/ad-search/src/main/java/com/durex/ad/controller/SearchController.java
+++ ad-service/ad-search/src/main/java/com/durex/ad/controller/SearchController.java
@@ -5,6 +5,9 @@ import com.durex.ad.annotation.IgnoreResponseAdvice;
 import com.durex.ad.client.SponsorClient;
 import com.durex.ad.client.vo.AdPlan;
 import com.durex.ad.client.vo.AdPlanGetRequest;
+import com.durex.ad.search.ISearch;
+import com.durex.ad.search.vo.SearchRequest;
+import com.durex.ad.search.vo.SearchResponse;
 import com.durex.ad.vo.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,11 +26,21 @@ public class SearchController {
 
     @Resource
     private SponsorClient sponsorClient;
+    @Resource
+    private ISearch iSearch;
 
     @IgnoreResponseAdvice
     @PostMapping("/getAdPlans")
     public CommonResponse<List<AdPlan>> getAdPlanByIds(@RequestBody AdPlanGetRequest adPlanGetRequest) {
         log.info("ad-search: getAdPlanByIds -> {}", JSON.toJSONString(adPlanGetRequest));
         return sponsorClient.getAdPlanByIds(adPlanGetRequest);
+    }
+
+    @PostMapping("/fetchAds")
+    public SearchResponse fetchAds(@RequestBody SearchRequest request) {
+
+        log.info("ad-search: fetchAds -> {}",
+                JSON.toJSONString(request));
+        return iSearch.fetchAds(request);
     }
 }
